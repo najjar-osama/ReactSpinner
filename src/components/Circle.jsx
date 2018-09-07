f7cf4fd780b7b70dfe7b4a24957f9fb3d0d9cb5f
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-08 01:10:59 +0200

    finalize spinner

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 
-
-const Circle = (props)=>{
-    const color = props.color === "yellow"? "circle--yellow" : "circle--green";
-    const circleButton = props.isButton || false ? "circle-button" : "";
-    let circleShadow = "";
-    if(props.shadow){
-        circleShadow = props.shadow === "type1" ? "circle-shadow-1" :"circle-shadow-2";
+const Circle = props => {
+  const color = props.color === "yellow" ? "circle--yellow" : "circle--green";
+  const circleButton = props.isButton || false ? "circle-button" : "";
+  let circleShadow = "";
+  if (props.shadow) {
+    circleShadow =
+      props.shadow === "type1" ? "circle-shadow-1" : "circle-shadow-2";
+  }
+  let wingBladePosition = "";
+  if (props.wingBladePosition) {
+    switch (props.wingBladePosition) {
+      case "top":
+        wingBladePosition = "wing-blade-top";
+        break;
+      case "left":
+        wingBladePosition = "wing-blade-left";
+        break;
+      case "right" :
+        wingBladePosition = "wing-blade-right";
+        break;
+      default :
+        wingBladePosition = "";
     }
-    return(<div className={`circle ${color} ${circleButton} ${circleShadow}`}>
-    </div>);
-}
+  }
+  return <div className={`circle ${color} ${circleButton} ${circleShadow} ${wingBladePosition}`} />;
+};
 
 export default Circle;
--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,25 +2,55 @@ import React from "react";
 
 import Circle from "./Circle";
 
-const Spinner = () => (
-  <div className="spinner-root">
-    <div className="container">
-      <div className="flex flex-align--center flex-content--center">
-        <div className="spinner-wrapper">
-          <div className="spinner-row spinner-row--top">
-            <Circle shadow="type1"/>
-          </div>
-          <div className="spinner-row spinner-row--bottom">
-            <Circle shadow="type1"/>
-            <Circle shadow="type2"/>
-          </div>
-          <div className="spinner-button-block">
-            <Circle color="yellow" isButton={true}/>
+class Spinner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.spin = this.spin.bind(this);
+    this.state = {
+      spinState: {
+        rotateDeg: 50
+      },
+      spinStyle: {
+        transform: "rotate(50deg)"
+      }
+    };
+  }
+
+  spin() {
+    const currentRotationDegree = this.state.spinState.rotateDeg;
+    const nextRotationDegree = currentRotationDegree + 720;
+    const nextRotationStyle = `rotate(${nextRotationDegree}deg)`;
+    this.setState(() => ({
+      spinState: {
+        rotateDeg: nextRotationDegree
+      },
+      spinStyle: {
+        transform: nextRotationStyle
+      }
+    }));
+  }
+  render() {
+    return (
+      <div className="spinner-root">
+        <div className="container">
+          <div className="flex flex-align--center flex-content--center">
+            <div className="spinner-wrapper" style={this.state.spinStyle}>
+              <div className="spinner-row spinner-row--top">
+                <Circle shadow="type1" wingBladePosition="top" />
+              </div>
+              <div className="spinner-row spinner-row--center" onClick={this.spin}>
+                <Circle color="yellow" isButton={true}/>
+              </div>
+              <div className="spinner-row spinner-row--bottom">
+                <Circle shadow="type1" wingBladePosition="left" />
+                <Circle shadow="type2" wingBladePosition="right" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Spinner;
