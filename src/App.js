5c8353df39b16f4d5d7ea69b9c73228f94937b08
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-08 01:36:50 +0200

    created app header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Spinner from "./components/Spinner";
-
+import Header from "./components/Header";
 class App extends Component {
   render() {
     return (
       <div className="layout-wrapper">
         <div className="layout-vp--full-height">
           <div className="main-content">
+            <Header />
             <Spinner />
           </div>
         </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => (
+  <div className="container">
+    <div className="header-wrapper">
+      <h1 className="header">
+        React Spinner
+      </h1>
+    </div>
+  </div>
+);
+
+export default Header;
