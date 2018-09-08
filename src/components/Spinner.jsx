77b1e9f43584a9424d8b24feeef22b506fb1d3db
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-08 02:35:46 +0200

    set spinner control panel actions

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -7,28 +7,47 @@ class Spinner extends React.Component {
   constructor(props) {
     super(props);
     this.spin = this.spin.bind(this);
+    this.toggleTurbo = this.toggleTurbo.bind(this);
     this.state = {
       spinState: {
         rotateDeg: 50
       },
       spinStyle: {
         transform: "rotate(50deg)"
+      },
+      turbo: {
+        mode : "turbo-off",
+        active : false
       }
     };
   }
 
   spin() {
-    const currentRotationDegree = this.state.spinState.rotateDeg;
-    const nextRotationDegree = currentRotationDegree + 720;
-    const nextRotationStyle = `rotate(${nextRotationDegree}deg)`;
-    this.setState(() => ({
-      spinState: {
-        rotateDeg: nextRotationDegree
-      },
-      spinStyle: {
-        transform: nextRotationStyle
+    this.setState((state) => {
+      const currentRotationDegree = state.spinState.rotateDeg;
+      const nextRotationDegree = currentRotationDegree + 720;
+      const nextRotationStyle = `rotate(${nextRotationDegree}deg)`;
+      return {
+        spinState: {
+          rotateDeg: nextRotationDegree
+        },
+        spinStyle: {
+          transform: nextRotationStyle
+        }
       }
-    }));
+    });
+  }
+  toggleTurbo (){
+    this.setState((state)=>{
+      const nextTurboMode = state.turbo.active ? "turbo-off" : "turbo-on";
+      const nextActiveState = !state.turbo.active;
+      return {
+        turbo: {
+          mode : nextTurboMode,
+          active : nextActiveState
+        }
+      }
+    });
   }
   render() {
     return (
@@ -36,7 +55,7 @@ class Spinner extends React.Component {
         <div className="container">
           <div className="flex flex-align--center flex-content--center">
             <div className="spinner-wrapper" >
-              <div className="spinner-body" style={this.state.spinStyle}>
+              <div className={`spinner-body ${this.state.turbo.mode}`} style={this.state.spinStyle}>
                 <div className="spinner-row spinner-row--top">
                   <Circle shadow="type1" wingBladePosition="top" />
                 </div>
@@ -51,9 +70,9 @@ class Spinner extends React.Component {
                   <Circle shadow="type2" wingBladePosition="right" />
                 </div>
               </div>
-              <SpinnerControlPanel spin={this.spin}/>
+              <SpinnerControlPanel spin={this.spin} turboIsActive={this.state.turbo.active} toggleTurbo={this.toggleTurbo} />
             </div>
-          </div>
+          </div> 
         </div>
       </div>
     );
--- src/components/SpinnerControlPanel.jsx
+++ src/components/SpinnerControlPanel.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
-
-const SpinnerControlPanel = (props)=>(
-    <div className="spinner-control-panel-wrapper">
-        <div className="button-group">
-            <button className="btn" onClick={props.toggleTurbo}>Turbo</button>
-            <button className="btn" onClick={props.spin}>Spin!</button>
-        </div>
+const SpinnerControlPanel = props => (
+  <div className="spinner-control-panel-wrapper">
+    <div className="button-group">
+      <button
+        className={`btn ${props.turboIsActive ? "turbo-active" : ""}`}
+        onClick={props.toggleTurbo}
+      >
+        Turbo
+      </button>
+      <button className="btn" onClick={props.spin}>
+        Spin!
+      </button>
+      
     </div>
-)
+    {props.turboIsActive?<p className="status-bar" >Turbo is now active!</p>:""}
+  </div>
+);
 
 export default SpinnerControlPanel;
